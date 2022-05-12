e28f20579c34e58fd80a530e3fcd155f61782b6a
Dongju <dannyldj@naver.com>
2022-05-12 12:07:11 +0900

    Add Swagger Fix Configs

--- Daekage_Server/Startup.cs
+++ Daekage_Server/Startup.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 using Daekage_Server.Models;
 using Daekage_Server.Services;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace Daekage_Server
 {
@@ -36,12 +37,21 @@ namespace Daekage_Server
 
             services.AddSingleton<NoticeService>();
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => options.UseMemberCasing());
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
